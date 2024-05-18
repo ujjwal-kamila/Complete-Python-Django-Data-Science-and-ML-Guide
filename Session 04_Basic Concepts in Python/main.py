022cf07962a8625fc13ff16206535cf1ae897146
Ujjwal <ujjwalkamila86@gmail.com>
2024-05-19 02:47:10 +0530

    python codes

--- Session 04_Basic Concepts in Python/main.py
+++ Session 04_Basic Concepts in Python/main.py
@@ -8,15 +8,35 @@ b = 10
 sum = a+b
 print(type(sum))
 print("Sum of " ,a, "and" ,b, " is ",sum)
+print(-20)
 
 # for string data type
 str = "Ujjwal"
 print(type(str))
 print("My name is : ",str)
 
+# for boolean data type 
+# starts with Capital 
+print(True)
+print(False)
+
 # for list data type 
 a = []
 print(type(a))
 print(a)
+# another list
 b =[1,2,3,4,5]
-type(b)
+type(b )
+
+
+# dictionary
+dict = {}
+print(dict)
+dict1 = {'name': "Ujjwal",'title':"Kamila"}
+print(dict1)
+
+#add more element by comma (,) seperate
+dict1 = {'name': "Ujjwal",
+        'title':"Kamila",
+        'age':'18',
+        'canvote':'Yes'}
---
+++ Session 05_  Introduction to Functions and Built in Functions/main.py
@@ -0,0 +1,11 @@
+# Session 05_  Introduction to Functions and Built in Functions
+
+# Built in functions 
+print("Hello")
+
+# to check build in of not 
+print(print)
+
+# taking user input with built in fun input()
+name = input("Enter Your name : ")
+print("Your namne is : ",name)
