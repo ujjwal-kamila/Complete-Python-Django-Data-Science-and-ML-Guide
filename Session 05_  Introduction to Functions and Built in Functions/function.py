afff6e002050f9b12620f8c4bf9c3197c540b9f4
Ujjwal <ujjwalkamila86@gmail.com>
2024-05-19 15:51:34 +0530

    Basic Functions in Python

---
+++ Session 05_  Introduction to Functions and Built in Functions/function.py
@@ -0,0 +1,27 @@
+# Basic Function defination and usage
+
+# def hello():
+#     print("Hello World!")
+
+# hello()
+
+
+# def hello(name):
+#     print("Hii")
+#     print(name)
+#     print("How are You ")
+# hello("Ujjwal")
+
+
+
+
+def add_nums() :
+    a = int(input("Enter a: "))
+    b = int(input("Enter b: "))
+    sum_result = a + b
+    print(f"Sum of {a} + {b} is: ",sum_result)
+    return sum_result
+
+add_nums( )
+
+
