e0ce1740f9dff2b1ca00970cece50cfdd1f174d7
Ujjwal <ujjwalkamila86@gmail.com>
2024-05-23 01:36:08 +0530

    dir() function built in modules

--- Session 05_  Introduction to Functions and Built in Functions/15_dir_fun.py
+++ Session 05_  Introduction to Functions and Built in Functions/15_dir_fun.py
@@ -1,5 +1,16 @@
 # 15. Practice - Using the built-in dir() Function
 
 
-# prints all the 
+# Prints all the names in the current local scope
 print(dir())
+
+# Prints the value of the __name__ variable
+print(__name__)
+
+# Prints the value of the __builtins__ module
+print(__builtins__)
+
+# Prints all the names of the attributes and methods in the __builtins__ module
+print(dir(__builtins__))
+
+
