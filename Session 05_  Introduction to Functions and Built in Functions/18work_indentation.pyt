9a00584ac7aaa75c70cc78888664ffc969faa0f1
Ujjwal <ujjwalkamila86@gmail.com>
2024-05-25 01:05:39 +0530

    Pep 8 documentation

--- Session 05_  Introduction to Functions and Built in Functions/18work_indentation.pyt
+++ Session 05_  Introduction to Functions and Built in Functions/18work_indentation.pyt
@@ -1,6 +1,8 @@
 # 18. Practice - Working with Indentations
 
 def print_num(number):
+    
+    
     print("The number is : ",number)
 
 print_num(5)
---
+++ Session 05_  Introduction to Functions and Built in Functions/19_pep8.py
@@ -0,0 +1,12 @@
+# 19. Following PEP 8 Guidelines
+
+# it is a style guide for Python code 
+
+# website link "https://peps.python.org/pep-0008/"
+
+print(
+    "Ujjwal" , 22100110162
+
+)
+
+
