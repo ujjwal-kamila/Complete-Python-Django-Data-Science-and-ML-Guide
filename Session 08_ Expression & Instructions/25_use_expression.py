a483496a52b830a82efc2ec4947666ceeb2b01ef
Ujjwal <ujjwalkamila86@gmail.com>
2024-05-29 23:39:37 +0530

    # 25. Practice - Using Expressions

--- Session 08_ Expression & Instructions/25_use_expression.py
+++ Session 08_ Expression & Instructions/25_use_expression.py
@@ -1,2 +1,32 @@
 # 25. Practice - Using Expressions
 # Use of Expressions 
+
+a = 10/ 4
+
+print (a)
+
+
+# another print function 
+res = print()
+print(res)
+
+# using built in input functions
+
+num = int(input("Enter any Number: "))
+print("Number is : " , num )
+
+print(type(num))
+
+# any expression produces a value that reuable later 
+# function decllaration is also a statement and not expression for Example :
+
+def my_fun():
+    print("Test")
+
+# print(def my_fun():
+#     print("Test"))  ##gives an error
+
+#we get same warning as 
+# print(if True:
+#     print('Test'))
+
