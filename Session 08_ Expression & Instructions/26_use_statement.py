715b052e7f38fd88996631e1a20d2abfabf3a252
Ujjwal <ujjwalkamila86@gmail.com>
2024-05-30 12:40:48 +0530

    updated # 26. Practice - Using Statements

--- Session 08_ Expression & Instructions/26_use_statement.py
+++ Session 08_ Expression & Instructions/26_use_statement.py
@@ -1,1 +1,24 @@
-# 
+# 26. Practice - Using Statements : perform some actions
+
+a = 20 # sample of statement
+
+# import functions 
+import datetime
+
+y = datetime.MAXYEAR
+print("Maximum years is :",y)
+
+
+if 10 > 9 :
+    print("True")    
+
+
+## TASK : 
+# crete any function and use inside of it a return statement
+def square(num):
+    res = num ** 2 
+    return res
+
+num= 5
+square_val = square(num)
+print(f"The square value of {num} is:",square_val)
