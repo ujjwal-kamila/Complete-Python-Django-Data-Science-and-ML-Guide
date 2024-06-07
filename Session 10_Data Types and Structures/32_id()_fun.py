7f39ea5d12892faba7700df12d7b2608a5552f40
Ujjwal <ujjwalkamila86@gmail.com>
2024-06-07 22:55:37 +0530

    32. Practice - Using the built-in id() Function

--- Session 10_Data Types and Structures/32_id()_fun.py
+++ Session 10_Data Types and Structures/32_id()_fun.py
@@ -3,7 +3,17 @@
 # Printing id of print function 
 print(id(print))
 
-
 # Crate a var and print id of that
 my_name = "Ujjwal"
 print()
+
+# Print addresses
+print("Address of a:", id(my_name))
+
+num = 1000
+print("Address is num is  :",id(num))
+
+# the address will remain same as upper addres
+other_num = num 
+print("Address of other num ",id(other_num))
+
