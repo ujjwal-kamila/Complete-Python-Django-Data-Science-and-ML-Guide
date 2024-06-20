32d55fba0c2d6d580292e71d4a1dc4a000ea1d25
Ujjwal <ujjwalkamila86@gmail.com>
2024-06-20 23:31:11 +0530

    32. Practice - Using the built-in id() Function

---
+++ Session 10_Data Types and Structures/33_classes.py
@@ -0,0 +1,19 @@
+# 33. Practice - Exploring Core Data Classes (str, int, bool, list, dict)
+
+my_num = 888
+# Check int or not 
+print(type(my_num)== int )
+
+print(type(int)) # <class 'type'>
+
+# Check for all other classes
+print(type(str))
+print(type(bool))
+print(type(list))
+print(type(dict))
+
+# All have same output : ""
+
+# Verify if the type of input is a string
+print(type('Ujjwal') == str) 
+print(type(33) == int) 
---
+++ Session 10_Data Types and Structures/34_builtin.py
@@ -0,0 +1,7 @@
+# 34. Practice - Using the built-in isinstance() Function
+
+# Check given input if instance or not 
+print(isinstance(555, int))
+print(isinstance('Ujjwal', str))
+# print(isinstance('Hello' == int)) # Error
+
