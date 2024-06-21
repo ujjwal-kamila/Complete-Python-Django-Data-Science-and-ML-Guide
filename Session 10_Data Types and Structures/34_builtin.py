4fdac18b5adbab00ee76c8b7dcf394141ed6b2cd
Ujjwal <ujjwalkamila86@gmail.com>
2024-06-21 23:16:32 +0530

    34. Practice - Using the built-in isinstance() Function

--- Session 10_Data Types and Structures/34_builtin.py
+++ Session 10_Data Types and Structures/34_builtin.py
@@ -5,3 +5,9 @@ print(isinstance(555, int))
 print(isinstance('Ujjwal', str))
 # print(isinstance('Hello' == int)) # Error
 
+# Object is a root class for all objects let's example
+
+print(isinstance('Ujjwal',object))   # True
+print(isinstance(787,object))   # True
+
+print(type(object))
