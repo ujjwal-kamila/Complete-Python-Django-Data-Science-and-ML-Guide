07412f9ca28bd65e0f606c8d20d65cc2710c878b
Ujjwal <ujjwalkamila86@gmail.com>
2024-06-22 23:18:48 +0530

    36. Practice - String Manipulation & Escape Characters

--- 11_Strings/36_strmanipulate.py
+++ 11_Strings/36_strmanipulate.py
@@ -1,2 +1,20 @@
-# 36. Practice - String Manipulation
+# 36. Practice - String Manipulation & Escape Characters 
+
+# create a str 
+long_str = "It\'s a Rainy day today"
+# We have to use escape characters '\' like :
+long_str = 'It\'s a Rainy day today'
+
+# also to split a str we have to put a backslash '\'
+long_str1 = "Hii "\
+    "This is "\
+        "a very long "\
+            "string."
+print(long_str1)
+
+print(long_str)
+print(type(long_str))
+print(type(long_str)== str)
+print(id(long_str))
+
 
