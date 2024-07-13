22485fa8f4837d458b41fd2abb2f69dfa308a68a
Ujjwal <ujjwalkamila86@gmail.com>
2024-07-14 00:28:36 +0530

    updated codes f-strings Concatenating Strings

--- 12_String Concatenation/38_main.py
+++ 12_String Concatenation/38_main.py
@@ -1,1 +1,17 @@
 # 38. String Concatenation
+
+# Sample example 
+a = "Hello"
+b = "world"
+greeting = a + ' ' + b 
+print(greeting)
+
+
+# lets make another example
+name = 'Ujjwal'
+hobby = "Play "
+time = "9"
+info = name + ' like to ' + hobby +'at ' + time  +' o\'clock'
+print(info)
+
+
