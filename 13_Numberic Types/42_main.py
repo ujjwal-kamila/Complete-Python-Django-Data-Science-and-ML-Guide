febea0c82a1c1da04f94a556094ce786300bd74b
Ujjwal <ujjwalkamila86@gmail.com>
2024-07-15 23:14:41 +0530

    Integers Manipulation

---
+++ 13_Numberic Types/42_main.py
@@ -0,0 +1,37 @@
+# 42.  Integers
+
+# # Example
+# num = 10 
+# print(num)
+# print(type(num))    # <class 'int'>
+
+# # user input 
+# number = input("Enter any number : ")
+# print(number)
+# print(type(number))  # <class 'str'>
+
+# # how to convert it to int 
+# usr_num = input ("Enter any number :")
+# any_num = int(usr_num)
+# print(any_num)
+# print(type(any_num))
+
+# # or we can a;so use int(input("Enter number : "))
+# input = int(input("Enter Number :"))
+# print(type(input))
+
+
+# power function to get the power for ex:
+base = 5 
+power = 3
+print(pow(base,power))
+print(base**power)
+
+
+
+# Long numebers 
+one_milion = 1_000_000
+print(one_milion)
+
+my_num = 3_456
+print(my_num)
---
+++ 13_Numberic Types/43_practice_int.py
@@ -0,0 +1,19 @@
+# 43. Practice - Integers Manipulation
+
+# We can do all arithemetci operations such as below:
+my_num = 10_000
+other_num = 5_000
+sum = my_num + other_num
+sub = my_num - other_num
+print("Sum is : ",sum)
+print("Subtraction is : ",sub)
+
+# converting str to int 
+# Taking user input 
+input_str = input("Eabcnter any numbers : ")
+print(type(input_str))    # <class 'str'>
+
+# converting into str
+input_num = int(input_str)
+print(type(input_num))    #<class 'int'>
+
