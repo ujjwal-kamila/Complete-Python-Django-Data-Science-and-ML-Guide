14b7abdcef8df231c80ed6700bcaf364e9f4af3b
Ujjwal <ujjwalkamila86@gmail.com>
2024-07-20 01:05:27 +0530

    Magic Methods

--- 14_Boolean Types/49_type_conversion.py
+++ 14_Boolean Types/49_type_conversion.py
@@ -18,3 +18,42 @@ print(float_num.__radd__(int_num))   # 8.14
 bool_val = True
 int_val = 10
 print(f"Sum of {bool_val} and {int_val} is :",bool_val+int_val)
+print('\n\n')
+
+
+# 50.Magic Methods 
+
+# all magic operators in python
+'''
+    __add__()
+    __eq__()
+    __and__()
+    __str__()
+    __neq__()
+    __or__()
+    
+'''
+
+# 51. Practice - Utilizing Magic Attributes and Methods
+my_str = 'ABC'
+# Print all available methods and attributes of an object
+# Example with a string object:
+# print(dir(my_str))
+
+# Print all methods of the str class using "dir(str)"
+# print(dir(str))
+
+# Print the documentation of the str class
+print(str.__doc__)
+# Same as bool
+print(bool.__doc__)
+
+# Comparison between two numbers 
+num = 100
+other_num = 100
+print(num.__eq__(other_num))
+# Same using comparison operator 
+print(num == other_num)
+
+# To get help or descrption of the methods "help()"
+print(help(num.__add__))
