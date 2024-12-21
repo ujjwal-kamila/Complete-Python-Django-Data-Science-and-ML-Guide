62e212aacb6cf2f65af7399be795ebc1c5c6d4cd
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2024-12-21 22:28:53 +0530

    updated codes

---
+++ 90_practice_fun.py
@@ -0,0 +1,76 @@
+# 90. Practice - Using Mutable and Immutable Objects as Function Arguments
+def print_fav_fruits (person,fruit):
+    # fruits_cpy = fruits.copy()
+    print(id(fruits))
+    fruits.pop()
+    for fruit in fruits :
+        print(f"{person} likes {fruit}")
+    
+name = 'Ujjwal'
+fruits = ['Apples','Oranges','Bananas']
+
+# fun call 
+print_fav_fruits(name , fruits)
+# if mutable the list 
+print(id(fruits))   #same object
+
+
+# 91. Practice - Mandatory and Optional Positional Arguments
+
+def greet(name, age):
+    print(f"Hello, {name}! You are {age} years old.")
+
+# Calling the function
+greet("Ujjwal", 20)  # Correct
+# greet("Ujjwal")      # TypeError
+
+# another type 
+def greet(name, age=18):  # 'age' is an optional
+    print(f"Hello, {name}! You are {age} years old.")
+
+# Calling the function
+greet("Ujjwal", 21)  # Hello, Ujjwal! You are 21 years old.
+greet("Ujjwal")      # Hello, Ujjwal! You are 18 years old. (uses default value)
+
+
+# ---------- TASK -----------
+'''
+1.Create a merge_lists_to_dict function
+2.The function must have two parameters 
+3.The function should combine two lists using the built in zip function 
+4.Conver a zip object to a dictionary and return it from the function 
+5.Call a function by passing it two lists as arguments 
+6.Output the result of the function call to the terminal '''
+
+# Create a merge_lists_to_dict function
+def merge_lists_to_dict(list1,list2):
+    zip_res= zip(list1,list2)
+    result = dict(zip_res)
+    return result
+
+# Create two lists as Key Value 
+l1 = ["name",'branch','roll',"age","Is_adult"]
+l2 = ["Ujjwal","CSE",22033,20,True]
+res = merge_lists_to_dict(l1,l2)
+
+# Output of result
+print("Combine Dict is :",res)
+
+
+
+# 93. Function Arguments
+# can add more arguments using .args in function parameter
+def sum_nums(*args):
+    print(args)
+    print(type(args))
+    return sum(args)
+
+print(sum_nums(5,6,7))
+
+# Create a fun as two args that person name ans post numbers
+def fun_post (name,posts_num):
+    info =print(f"{name} wrote {posts_num} posts in Linkedin")
+    return info 
+fun_post("Ujjwal",5)
+
+
---
+++ 94_args.py
@@ -0,0 +1,61 @@
+# 94. Practice - Using *args to Gather Positional Arguments into a Tuple
+
+def sort_nums(*args):
+    # print(args)
+    for arg in args:
+        print(arg)
+    return sorted(args)
+        
+sorted_nums = sort_nums(3,6,1,89)
+print("sorted nums :",sorted_nums)
+
+
+sorted_nums = sort_nums(100,8,9,5,2,1)
+print("sorted nums :",sorted_nums)
+
+
+# 95. Keyword Arguments
+# sample ex
+def get_info(name,address):
+    info = f"{name} residing in {address}"
+    return info 
+
+# for more readable use keyword args
+print_info = get_info(name = 'Ujjwal',address='Kalyani')
+print(print_info)
+
+# merge args to a dict
+def get_person_info(**person):
+    print(person)
+    print(type(person))
+    
+    info = (
+        f"{person['name']} located at "
+        f"{person['address']}"
+    )
+    return info
+    
+info = get_person_info(name = 'Ujjwal',address='Kalyani')
+print(info)
+
+
+
+# 96. Practice - Working with Keyword Arguments
+def post_info(name,commit_qty,day):
+    res = f'{name} commits {commit_qty} Codes on {day}'
+    return res
+
+# way to do fun call as below ex
+info = post_info('Ujjwal',5,'Monday')
+print(info)
+
+# wrong way 
+info = post_info('Monday','Ujjwal',5)
+print(info)        # wrong output
+
+# To tackle this we have to use keyword args as ex
+info1 = post_info(name='Ujjwal',commit_qty=5,day='Monday')
+info2 = post_info(commit_qty=10,day='Sunday',name='Ujjwal')    # for keyword args order doesn't matter 
+print(info1)
+print(info2)
+
