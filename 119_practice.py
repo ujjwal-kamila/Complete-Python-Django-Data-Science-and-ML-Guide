252390d02eac4e2d20ca78e262fecba4affa9397
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2025-05-27 23:46:52 +0530

    create a fun that checks the dict is empty or not and value is present or not

--- 119_practice.py
+++ 119_practice.py
@@ -21,3 +21,28 @@ obj2 = MyObject([1, 2])
 
 print(bool(obj1))
 print(bool(obj2))
+print()
+print()
+
+
+# create a fun that checks the dict is empty or not and value is present or not
+def check_dict(my_dict,val_to_find):
+    if not my_dict:
+        print("The dictionary is empty..")
+    else:
+        print("The dictionary is not empty..")
+        if val_to_find in my_dict.values():
+            print(f"The Value '{val_to_find}' is present is dictionary..")
+        else:
+            print(f"The Value '{val_to_find}' is NOT present is dictionary..")
+
+info = {
+    'name': 'Ujjwal',
+    'age': 21    
+}
+
+check_dict(info,'Ujjwal')
+check_dict(info,21)
+
+d3 = {}
+check_dict(d3, "anything")
