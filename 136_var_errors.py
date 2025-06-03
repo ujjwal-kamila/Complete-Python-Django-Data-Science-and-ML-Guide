993cc71f800ecff4eb26b45662a1e591bec597b3
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2025-06-04 00:24:16 +0530

    Handling Undefined Variable Errors

---
+++ 136_var_errors.py
@@ -0,0 +1,35 @@
+# 136. Example - Handling Undefined Variable Errors
+
+# Trying to print a variable that is not defined
+try:
+    print(value)  
+
+except NameError:
+    print("Error: The variable is not defined!")
+
+except Exception as e:
+    print("Some other error occurred:", e)
+
+else:
+    print("Variable printed successfully!")
+
+finally:
+    print("Program execution completed.")
+
+
+# in finally block always close the file 
+try:
+    file = open("file.txt", "r")  
+except FileNotFoundError as e:
+    print(e) 
+else:
+    print("File is ready for reading")
+    print("File contains... \n")
+    print(file.read())
+finally:
+    # In finally block, always try to close the file if it was opened
+    if 'file' in globals() and file and not file.closed:
+        file.close()
+        print("\n...........")
+        print("File was closed.")
+        print(file.closed)
