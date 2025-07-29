a6722d5cb2e3127657cfb35d87cf42a982837494
Ujjwal Kamila <ujjwalkamila86@gmail.com>
2025-07-29 21:07:34 +0530

    Practice - Unpacking Tuples

---
+++ 142_unpack_tuple.py
@@ -0,0 +1,20 @@
+# 142. Practice - Unpacking Tuples
+
+info = ('ujjwal',21,"Junior Developer ")
+
+# unpacking
+name,age,position = info
+print(name,age,position)
+
+
+# basic rgb tuple unpacking
+rgb = (255, 100, 50)
+
+r, g, b = rgb  
+
+print("Red:", r)
+print("Green:", g)
+print("Blue:", b)
+
+color = (r,g,b)
+print(color)
